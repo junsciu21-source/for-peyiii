bb698802338d5698c8e2a4a2d8f7529bb34a40ea
junsciu21-source <juns.ciu21@gmail.com>
2025-10-19 10:25:46 +0700

    Update script11.js

--- script11.js
+++ script11.js
@@ -615,7 +615,7 @@ function loadSpotifyPlaylist(playlistNumber) {
         },
         3: {
             // Ganti dengan playlist ketiga kamu
-            embedUrl: 'https://open.spotify.com/embed/playlist/2ittdGvMZFWgIGIHo7lMTf',
+            embedUrl: 'https://open.spotify.com/embed/playlist/2ittdGvMZFWgIGIHo7lMTf?utm_source=generator',
             name: 'Your Fav Songs :3',
             description: 'From Your Playlistt 🌟'
         }
@@ -1185,3 +1185,4 @@ function handleContinueNavigation() {
     }
 
 }
+
